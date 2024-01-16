9be78a0fe28ea9288adb894913e2634bc5e73593
Kemal Zulfiqar <kemalzulfiqar@Kemals-MacBook-M1-Pro.local>
2024-01-17 03:47:02 +0700

    fix regex

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,10 @@ function App() {
       rules: [{ required: true, message: "This field is required!" }],
     },
     //PASSWORD
-    // regex ref :
     // Minimum eight characters, at least one letter and one number: "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
     // Minimum eight characters, at least one letter, one number and one special character: "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
     // Minimum eight characters, at least one uppercase letter, one lowercase letter and one number: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
-    // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+    // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character: "(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
 
     {
       name: "password",
